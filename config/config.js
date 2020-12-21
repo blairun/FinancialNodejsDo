882acc9e39b0d01c850c3a8bd4eca09a20b474b2
blairun <blairun@gmail.com>
2020-12-20 20:34:38 -0800

    db config

--- config/config.js
+++ config/config.js
@@ -27,7 +27,7 @@ module.exports = {
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres',
     // logging: false,
     dialectOptions: {
       ssl: {
